fcb6cf2a57677d54b8ff7f0616a8b4a84d482531
Rahul Baghel <rahulbaghel6513@gmail.com>
2022-04-21 12:11:50 +0530

    This is a first commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import React from "react";
 import './App.css';
+import { Question } from "./Question";
 
 function App() {
+  
+  const [curquestion, setcurquestion] = useState(0);
+  const[score,setscore]=useState(false);
+  const[score1,setscore1]=useState(0);
+  const handleanswer = (correct) => {
+     
+    const next = curquestion + 1;
+    if(next<Question.length)
+    {
+        setcurquestion(next);
+    }
+    else{
+      setscore(true);
+    }
+    
+    if(correct === true)
+    {
+       setscore1(score1+1);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container text-center  py-4">
+        <h3 className="top ">Quiz App</h3>
     </div>
+      <div className="app">   
+        <div >
+          {score ? (
+            <div className="score-section"> Your Score is {score1} out of {Question.length}</div>
+          ) : (
+            <>
+              <div className="question-section">
+              <h5>General Knowlegde Question</h5>
+                <div className="question-count">
+                  <span >Question :{curquestion+1}/</span> {Question.length}
+                </div>
+                <div className="question-text">
+                  {Question[curquestion].question}
+                </div>
+              </div>
+              <div className="answer-section">
+                {Question[curquestion].Answeroption.map((Answeroption) => (
+                  <button onClick={()=>handleanswer(Answeroption.correct)}>{Answeroption.answertext}</button>
+                ))}
+                
+              </div>
+             
+            </>
+          )}
+        
+        </div>
+      
+      </div>
+     
+    </>
   );
 }
 
---
+++ src/Question.js
@@ -0,0 +1,94 @@
+ export const Question = [
+    {
+      question: "What is the full form of CPU   ",
+      Answeroption: [
+        { answertext: "Computer process", correct: false },
+        { answertext: "Computer processing", correct: false },
+        { answertext: "Computer processed", correct: false },
+        { answertext: "Central processing unit", correct: true },
+      ],
+    },
+    {
+      question: "What is the full form of RAM",
+      Answeroption: [
+        { answertext: "Random access memory", correct: true },
+        { answertext: "Computer processing", correct: false },
+        { answertext: "Computer processed", correct: false },
+        { answertext: "Central unit memory", correct: true },
+      ],
+    },
+    {
+      question: "What is the full form of USA",
+      Answeroption: [
+        { answertext: "Union states", correct: false },
+        { answertext: "united America", correct: false },
+        { answertext: "untied states America", correct: false },
+        { answertext: "United States of America", correct: true },
+      ],
+    },
+    {
+      question: "Captial of Russia",
+      Answeroption: [
+        { answertext: "Taiwan", correct: false },
+        { answertext: "Moscow", correct: true },
+        { answertext: "England", correct: false },
+        { answertext: "Brazil", correct: false },
+      ],
+    },
+    {
+        question: "Who is the Present CM of Uttar pradesh",
+        Answeroption: [
+          { answertext: "M.k sharma", correct: false },
+          { answertext: "Jyoti Patel", correct: false },
+          { answertext: "Yogi Aditya Nath", correct: true },
+          { answertext: "Nono", correct: false },
+        ],
+      },
+      {
+        question: "What is the full form of AUKUS",
+        Answeroption: [
+          { answertext: "Australia,UK,US", correct: true },
+          { answertext: "America ,UK,US", correct: false },
+          { answertext: "Australia,Japan,india", correct: false },
+          { answertext: "United UK US", correct: false },
+        ],
+      },
+      {
+        question: "What is the full form of ECI",
+        Answeroption: [
+          { answertext: "Electrol of india", correct: false },
+          { answertext: "Election Commission Of India", correct: true },
+          { answertext: "Election commissioner of India", correct: false },
+          { answertext: "Election of Commission", correct: false },
+        ],
+      },
+      {
+        question: "What is the full form of UK",
+        Answeroption: [
+          { answertext: "Union states", correct: false },
+          { answertext: "united America", correct: false },
+          { answertext: "United Kingdom", correct: true },
+          { answertext: "United States of Kingdom", correct: false },
+        ],
+      },
+      {
+        question: "What is the full form of ROM",
+        Answeroption: [
+          { answertext: "Read only Memory", correct: true },
+          { answertext: "Read any mind", correct: false },
+          { answertext: "Random Memory ", correct: false },
+          { answertext: "Read of Mind", correct: false },
+        ],
+      },
+      {
+        question: "What is the full form of MP",
+        Answeroption: [
+          { answertext: "Member of Parliament", correct: true },
+          { answertext: "Member of Public", correct: false },
+          { answertext: "Member of Parliamentary ", correct: false },
+          { answertext: "Public Member", correct: true },
+        ],
+      },
+    
+     
+  ];
